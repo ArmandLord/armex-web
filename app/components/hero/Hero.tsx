de231267457c25788a1cced23805e8cdfe074fe1
José Armando Pérez Pérez <joperez@creditu.com>
2022-09-11 22:20:18 -0500

    optimizacion img

--- app/components/hero/Hero.tsx
+++ app/components/hero/Hero.tsx
@@ -3,20 +3,22 @@ import hero from "../../assets/hero_team.jpeg";
 
 const Hero = ({ children }: any) => {
   return (
-    <div className="bg-black w-full">
-      <div className="container mx-auto h-[100vh]">
-        {children}
-        <div
-          style={{
-            backgroundImage: `url(${hero})`,
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "center",
-            width: "100%",
-            height: "100%",
-            filter: "opacity(0.25)",
-          }}
-        ></div>
+    <div
+      style={{
+        backgroundImage: `url(${hero})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        width: "100%",
+        height: "100%",
+        // filter: "opacity(.5)",
+      }}
+      className="w-full"
+    >
+      <div className="bg-gradient-to-r from-black to-transparent w-full h-full">
+        <div className="bg-gradient-to-l from-black to-transparent w-full h-full">
+          <div className="container mx-auto h-[100vh]">{children}</div>
+        </div>
       </div>
     </div>
   );
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -11,8 +11,8 @@ export default function Index() {
       <div className="bg-secondary h-20 sticky top-0 p-5 drop-shadow shadow-blue-600"></div>
       <div className="bg-white w-full h-screen"></div>
       <footer className="h-96 p-20 bg-tertiary">
-          <h1 className="text-white text-4xl text-center p-20">Footer</h1>
-        </footer>
+        <h1 className="text-white text-4xl text-center p-20">Footer</h1>
+      </footer>
     </div>
   );
 }
